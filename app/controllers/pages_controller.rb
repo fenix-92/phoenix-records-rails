3c670250e30c952be54f4a2f51105f1284720463
fenix-92 <ignacio.granero@hotmail.com>
2019-04-20 03:42:52 +0200

    Login functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    # This could redirect the users to the login page if they log out
+    # for private websites or backends
+    # if !user_signed_in?
+    #  redirect_to new_user_session_path
+    # end
   end
 end
