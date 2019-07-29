b7eff55cdfa63e97d33c8ba7ffd5f344dc5a4e9c
fenix-92 <ignacio.granero@hotmail.com>
2019-07-29 14:37:29 +0200

    Material template

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require turbolinks
-//= require_tree .
-//= require bootstrap
 //= require rails-ujs
-//= require popper
-//= require bootstrap-sprockets
-//= require activestorage
 //= require turbolinks
-//= require toastr
+//= require jquery
+//= require jquery_ujs
+//= require popper
+//= require material/bootstrap-material-design
+//= require material/perfect-scrollbar.jquery.min
+//= require material/chartist.min
+//= require material/arrive.min
+//= require material/bootstrap-notify
+//= require material/material-dashboard
+//= require material/demo
+//= require_tree .
