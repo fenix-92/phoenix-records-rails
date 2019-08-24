0f98f166f18838132349fba6c2469b5cb2248ccd
fenix-92 <ignacio.granero@hotmail.com>
2019-08-24 15:11:44 +0200

    Blog model created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
+  end
+
+  def new
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'admin', to: 'admin#index'
   get 'blog', to: 'articles#index'
 
-  resources :posts, :artists, :labels, :formats
+  resources :posts, :artists, :labels, :formats, :articles
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
