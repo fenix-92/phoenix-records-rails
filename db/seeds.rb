ea398475359a66d15e012b5a3dba74b11c948bb4
phoenix-blaze-dev <ignacio.granero@hotmail.com>
2019-09-26 10:55:30 +0200

    Seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create! :email => 'admin@gmail.com', :password => 'admin1', :password_confirmation => 'admin1'
+artists = Artist.create([{ name: 'Def Leppard', discogsid: '252443'}])
