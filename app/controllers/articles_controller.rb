84cdf4f2778685827d335262f4937b0f9b072f6a
phoenix-blaze-dev <ignacio.granero@hotmail.com>
2019-10-14 15:00:08 +0200

    Added user control to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,29 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    check_user_logged
   end
 
   def show
+  end
+
+  def check_user_logged
+    if !user_signed_in?
+      redirect_to new_user_session_path, info: "Please, log in"
+    else
+      # User is logged
+    end
+  end
+
+  def create
+    check_user_logged
+    # render plain: params[:artist].inspect
+    @article = Article.new store_params
+    # @artist.avatar.attach(params[:avatar])
+    if @article.save
+      redirect_to admin_articles_path, success: "Article created"
+    else
+      render 'new', alert: "Article don't created"
+    end
   end
 end
