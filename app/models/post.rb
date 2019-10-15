7ee621fc018c872a8f4f9318d59b1e00872e7f84
phoenix-blaze-dev <ignacio.granero@hotmail.com>
2019-10-15 12:47:56 +0200

    Changed admin record table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   has_one_attached :front
   has_one_attached :back
+  has_one_attached :vinyl1
+  has_one_attached :vinyl2
   has_one_attached :audio_sample
   belongs_to :format
   belongs_to :artist
