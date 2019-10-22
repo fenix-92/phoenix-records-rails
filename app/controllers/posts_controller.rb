60c59cbe8aabf4e0afc654edf0a41fe099be9fd1
fenix-92 <ignacio.granero@hotmail.com>
2019-10-23 00:02:34 +0200

    Migration for first record

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -75,6 +75,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :artist_id, :country_id, :store_id, :format_id, :sleeve_grading_id, :media_grading_id, :record_company_id, :store_id, :year, :discogsid, :front, :back, :audio_sample, :vinyl1, :vinyl2, :q, :hex_color, :featured, :original_inner, :misprint, :double_lp, :unofficial, :colored, :first_press, :gatefold, :notes, :serial_no, :matrix_a, :matrix_b)
+    params.require(:post).permit(:title, :artist_id, :country_id, :store_id, :format_id, :sleeve_grading_id, :buyed_at, :media_grading_id, :record_company_id, :store_id, :year, :discogsid, :front, :back, :audio_sample, :vinyl1, :vinyl2, :q, :hex_color, :featured, :original_inner, :misprint, :double_lp, :unofficial, :colored, :first_press, :gatefold, :notes, :inshrink, :serial_no, :matrix_a, :matrix_b)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,15 @@
 user = User.create! :email => 'admin@admin.com', :password => 'admin1', :password_confirmation => 'admin1'
 
 # Formats
-format = Format.create([{ name: 'LP', long_name: 'Vinyl'},
-                        { name: '7"', long_name: 'Single'},
-                        { name: 'Cassette', long_name: 'Cassette'},
-                        { name: 'Maxi', long_name: 'Maxi-Single'},
-                        { name: 'CD', long_name: 'CD'},
-                        { name: 'CD Single', long_name: 'CD Single'},
-                        { name: '10"', long_name: 'Shellac'},
-                        { name: 'Cassette Single', long_name: 'Cassette Single'}
-                        ])
+lp = Format.create( name: 'LP', long_name: 'Vinyl')
+seven_inch = Format.create( name: '7"', long_name: 'Single')
+cassette = Format.create( name: 'Cassette', long_name: 'Cassette')
+maxi = Format.create( name: 'Maxi', long_name: 'Maxi-Single')
+cd = Format.create(name: 'CD', long_name: 'CD')
+cd_single = Format.create(name: 'CD Single', long_name: 'CD Single')
+ten_inch = Format.create(name: '10"', long_name: 'Shellac')
+cassette_single = Format.create(name: 'Cassette Single', long_name: 'Cassette Single')
+
 
 # Artists
 defleppard = Artist.create( name: 'Def Leppard', discogsid: '252443')
@@ -53,30 +53,38 @@ rca = RecordCompany.create(name: 'RCA', long_name: 'RCA Records')
 rca.photo.attach(io: File.open("#{Rails.root}/resources/labels/rca.png"), filename: "rca.png")
 wea = RecordCompany.create(name: 'WEA', long_name: 'WEA Records')
 wea.photo.attach(io: File.open("#{Rails.root}/resources/labels/wea.png"), filename: "wea.png")
+iso_records = RecordCompany.create(name: 'ISO', long_name: 'ISO Records')
 
 # Gradings
-media = MediaGrading.create([{ name: '-', text: 'Not present'},
-                              { name: 'F', text: 'Fair'},
-                              { name: 'P', text: 'Poor'},
-                              { name: 'G', text: 'Good'},
-                              { name: 'G+', text: 'Good Plus'},
-                              { name: 'VG', text: 'Very Good'},
-                              { name: 'VG+', text: 'Very Good Plus'},
-                              { name: 'NM', text: 'Near Mint'},
-                              { name: 'M', text: 'Mint'}
-                              ])
-sleeve = SleeveGrading.create([{ name: '-', text: 'Not present'},
-                              { name: 'F', text: 'Fair'},
-                              { name: 'P', text: 'Poor'},
-                              { name: 'G', text: 'Good'},
-                              { name: 'G+', text: 'Good Plus'},
-                              { name: 'VG', text: 'Very Good'},
-                              { name: 'VG+', text: 'Very Good Plus'},
-                              { name: 'NM', text: 'Near Mint'},
-                              { name: 'M', text: 'Mint'},
-                              { name: 'S', text: 'Sealed'}
-                              ])
+media_not_present = MediaGrading.create(name: '-', text: 'Not present')
+media_fair = MediaGrading.create( name: 'F', text: 'Fair')
+media_poor = MediaGrading.create( name: 'P', text: 'Poor')
+media_good = MediaGrading.create( name: 'G', text: 'Good')
+media_good_plus = MediaGrading.create( name: 'G+', text: 'Good Plus')
+media_very_good = MediaGrading.create( name: 'VG', text: 'Very Good')
+media_very_good_plus = MediaGrading.create( name: 'VG+', text: 'Very Good Plus')
+media_near_mint = MediaGrading.create( name: 'NM', text: 'Near Mint')
+media_mint = MediaGrading.create( name: 'M', text: 'Mint')
 
-store = Store.create([{ name: 'Unknown'},
-                      { name: 'Discogs', description: 'Discogs is a website and crowdsourced database of information about audio recordings, including commercial releases, promotional releases, and bootleg or off-label releases. The Discogs servers, currently hosted under the domain name discogs.com, are owned by Zink Media, Inc., and are located in Portland, Oregon, US.', online: '1'}
-                      ])
+sleeve_generic = SleeveGrading.create( name: 'Generic', text: 'Generic')
+sleeve_not_present = SleeveGrading.create( name: 'Not Present', text: 'Not Present')
+sleeve_fair = SleeveGrading.create( name: 'F', text: 'Fair')
+sleeve_poor = SleeveGrading.create( name: 'P', text: 'Poor')
+sleeve_good = SleeveGrading.create( name: 'G', text: 'Good')
+sleeve_good_plus = SleeveGrading.create( name: 'G+', text: 'Good Plus')
+sleeve_very_good = SleeveGrading.create( name: 'VG', text: 'Very Good')
+sleeve_very_good_plus = SleeveGrading.create( name: 'VG+', text: 'Very Good Plus')
+sleeve_near_mint = SleeveGrading.create( name: 'NM', text: 'Near Mint')
+sleeve_mint = SleeveGrading.create( name: 'M', text: 'Mint')
+sleeve_sealed = SleeveGrading.create( name: 'S', text: 'Sealed')
+
+no_store = Store.create(name: 'No Store', online: 0)
+ebay = Store.create(name: 'eBay', online: 1)
+metralleta = Store.create(name: 'La Metralleta', address: 'Calle del Postigo de San Martín, 1, 28013', online: 0)
+discogs = Store.create( name: 'Discogs', description: 'Discogs is a website and crowdsourced database of information about audio recordings, including commercial releases, promotional releases, and bootleg or off-label releases. The Discogs servers, currently hosted under the domain name discogs.com, are owned by Zink Media, Inc., and are located in Portland, Oregon, US.', online: '1')
+sevilla_record = Store.create(name: 'Record Sevilla', address: 'Amor de Dios, 17, 41002 Sevilla', online: 0)
+
+
+no_plan = Post.create( title: 'No Plan', artist_id: bowie.id, year: '2016', country_id: europe.id, format_id: lp.id, store_id: sevilla_record.id, media_grading_id: media_near_mint.id, sleeve_grading_id: sleeve_near_mint.id, record_company_id: iso_records.id, discogsid: '10150675', serial_no: '88985419651', inshrink: 1, buyed_at: DateTime.parse("12/11/2017 11:58") )
+no_plan.front.attach(io: File.open("#{Rails.root}/resources/records/david_bowie_-_no_plan/front.jpg"), filename: "front.jpg")
+no_plan.back.attach(io: File.open("#{Rails.root}/resources/records/david_bowie_-_no_plan/back.jpg"), filename: "back.jpg")
