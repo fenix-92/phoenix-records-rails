a684fad8a7fa9fcf50b6ca4cdf99b8e0db1736d8
fenix-92 <ignacio.granero@hotmail.com>
2019-10-22 22:47:59 +0200

    Hover vinyl effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
 //= require turbolinks
 //= require core/bootstrap-material-design.min
