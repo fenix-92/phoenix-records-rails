d644a5c239037a58347812c22a164b8fab8f764b
fenix-92 <ignacio.granero@hotmail.com>
2019-12-02 15:46:19 +0100

    Disabled assets precompile on initialize

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module PhoenixRecords
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("app", "assets", "font", "roboto")
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
